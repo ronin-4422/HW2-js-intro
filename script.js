18c0d8f7d1cdb1f32973a0147486b15418e315d4
ronin-4422 <mmfesenko@gmail.com>
2021-06-24 21:10:36 +0300

    task 4

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log('------------- # 4')
+let a = 2;
+let b = 2;
+let minus = a - b;
+console.log(minus);
+let plus = a + b;
+console.log(plus);
+let multiply = a * b;
+console.log(multiply)
+let split = a / b;
+console.log(split);
+
+
